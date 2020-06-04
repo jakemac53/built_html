2a1e1e3a37df43262a999d1ba96c9ee1c7f968b1
Andrey Chernyshev <om08uhkn@gmail.com>
2020-06-04 19:26:09 +0300

    Add support for json extension (#10)

--- test/builder_test.dart
+++ test/builder_test.dart
@@ -6,13 +6,13 @@ import 'package:test/test.dart';
 main() {
   test('Digest command works', () async {
     await testBuilder(HtmlTemplateBuilder(), {
-      'a|index.template.html': '''
-<p>{{digest index.template.html}}</p>
-''',
+      'a|index.template.html': '<p>{{digest index.template.html}}</p>',
+      'a|script.js': 'useful js script',
+      'a|styles.css': 'useful css styles',
+      'a|assets.template.json': '{ "script.js": "{{digest script.js}}", "styles.css": "{{digest styles.css}}" }',
     }, outputs: {
-      'a|index.html': '''
-<p>9e43ec54624795b34b4dd9ee60ffe652</p>
-''',
+      'a|index.html': '<p>ddfbfb428acba99177a5d34f795f24d9</p>',
+      'a|assets.json': '{ "script.js": "5140897c4b2dd3628459f33e0df969e2", "styles.css": "d5e95fc0e4382d6f22d3b4103594da79" }'
     });
   });
 }
